4155ba466765f24d89ee1045773b7ec8b01d2173
wmgx <1109270538@qq.com>
2021-05-11 23:41:17 +0800

    修复事务跑出异常不回滚的错误

--- src/main/java/com/hanhai/cloud/base/BaseException.java
+++ src/main/java/com/hanhai/cloud/base/BaseException.java
@@ -7,7 +7,7 @@ import com.hanhai.cloud.constant.ResultCode;
  * @author wmgx
  * @create 2021-02-01-19:21
  **/
-public class BaseException extends Exception{
+public class BaseException extends RuntimeException{
     private Integer code;
     private String msg;
 
