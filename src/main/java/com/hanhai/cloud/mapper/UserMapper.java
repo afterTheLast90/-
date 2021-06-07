89c8a997d1abbc930b9fbac17d03b46f19a7ee76
wmgx <1109270538@qq.com>
2021-06-07 20:29:47 +0800

    补缺参数类

--- src/main/java/com/hanhai/cloud/mapper/UserMapper.java
+++ src/main/java/com/hanhai/cloud/mapper/UserMapper.java
@@ -13,8 +13,25 @@ import java.util.List;
  **/
 public interface UserMapper extends BaseMapper<User> {
 
-    @Select("select * from user where user_phone=#{p1} or user_email=#{p1} and deleted=false ")
+    @Select("select * from user where (user_phone=#{p1} or user_email=#{p1}) and deleted=false ")
     public User getUserByPhoneOrEmail(@Param("p1") String t1);
+
+    @Select("select * from user where user_phone=#{p1}  and deleted=false ")
+    public User getUserByPhone(@Param("p1") String t1);
+    @Select("select * from user where user_email=#{p1} and deleted=false ")
+    public User getUserByEmail(@Param("p1") String t1);
+
+
+
+
+    @Select("select * from user where (user_phone like concat('%',#{key} ,'%') or " +
+            "user_email like concat('%',#{key} ,'%')  or user_name  like concat('%',#{key} ,'%'))" +
+            "and deleted=false  ")
+    public List<User> selectUser(@Param("key") String key);
+
+
+    @Select("select * from user where deleted=false  ")
+    public List<User> getAll();
 
     // 根据 email/phone/name模糊搜索用户，不包自身
     @Select("select user_id, user_name, user_email, user_phone from user where " +
--- src/main/java/com/hanhai/cloud/service/UserService.java
+++ src/main/java/com/hanhai/cloud/service/UserService.java
@@ -2,9 +2,12 @@ package com.hanhai.cloud.service;
 
 import com.hanhai.cloud.base.BaseService;
 import com.hanhai.cloud.entity.User;
+import com.hanhai.cloud.params.UserSearchParams;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author wmgx
@@ -27,6 +30,30 @@ public class UserService extends BaseService {
     public User updateById(User user){
         userMapper.updateById(user);
         return  user;
+    }
+
+    public List<User> selectUser(UserSearchParams param){
+        startPage(param);
+        return userMapper.selectUser(param.getKey());
+    }
+
+    public List<User> getAll(){
+        return userMapper.getAll();
+    }
+    public User insertUser(User user){
+        userMapper.insert(user);
+        return user;
+    }
+
+    public Boolean checkUserPhone(String phone){
+        return userMapper.getUserByPhone(phone)==null  ;
+    }
+    public Boolean checkUserEmail(String email){
+        return userMapper.getUserByEmail(email)==null;
+    }
+    
+    public void deleteUser(Long userId){
+        userMapper.deleteById(userId);
     }
 
 
