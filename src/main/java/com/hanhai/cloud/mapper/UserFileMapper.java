2a3004c1b06fe9ad32923a0260726e96e675acfe
wmgx <1109270538@qq.com>
2021-06-13 23:16:03 +0800

    修改主页面文件显示顺序和more按钮绑定错误

--- src/main/java/com/hanhai/cloud/mapper/UserFileMapper.java
+++ src/main/java/com/hanhai/cloud/mapper/UserFileMapper.java
@@ -19,10 +19,12 @@ public interface UserFileMapper extends BaseMapper<UserFile> {
     @Select("select * from user_files where file_parent_path=#{path} and user_id=#{userId} and deleted = 0 ")
     public List<UserFile> getFilesByParent(@Param("path") String path,@Param("userId")  Long userId);
 
-    @Select("select * from user_files where file_parent_path=#{path} and user_id=#{userId} and file_type='DIR' and deleted=0 order by updated_time")
+    @Select("select * from user_files where file_parent_path=#{path} and user_id=#{userId} and " +
+            " file_type='DIR' and deleted=0 order by updated_time desc")
     public List<UserFile> getDir(@Param("path") String path,@Param("userId") Long userId);
 
-    @Select("select * from user_files where file_parent_path=#{path} and user_id=#{userId} and file_type!='DIR' and deleted=0 order by updated_time")
+    @Select("select * from user_files where file_parent_path=#{path} and user_id=#{userId} and " +
+            "file_type!='DIR' and deleted=0 order by updated_time desc")
     public List<UserFile> getFiles(@Param("path") String path,@Param("userId") Long userId);
 
     @Select("select * from user_files where file_parent_path=#{path} and file_name=#{name} and user_id=#{userId} and file_type=#{type} and deleted = 0 ")
